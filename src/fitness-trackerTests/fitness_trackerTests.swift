6c281af225fff69147337a911639b84e4bf2691d
Illia Khamza <khamzaillia@gmail.com>
2026-09-04 18:23:16 +0100

    Added startup swift project

---
+++ src/fitness-trackerTests/fitness_trackerTests.swift
@@ -0,0 +1,18 @@
+//
+//  fitness_trackerTests.swift
+//  fitness-trackerTests
+//
+//  Created by Taranova, Kseniia on 04/09/2026.
+//
+
+import Testing
+
+struct fitness_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
